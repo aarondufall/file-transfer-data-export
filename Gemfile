74d22472685afe23616d9ea6728cff2fcdce1899
aarondufall <aald212@gmail.com>
2017-03-16 09:08:32 -0700

    Add consumer and process host

--- Gemfile
+++ Gemfile
@@ -1,7 +1,11 @@
 source 'https://rubygems.org'
 
 gem 'eventide-postgres'
+gem 'evt-consumer-postgres'
+gem 'evt-component_host'
+
+gem 'file_transfer-client', github: "aarondufall/file-transfer-component"
+
 gem 'sequel'
 gem 'test_bench'
 gem 'pry'
-gem 'file_transfer-client', github: "aarondufall/file-transfer-component"
--- lib/file_transfer_data_export.rb
+++ lib/file_transfer_data_export.rb
@@ -1,8 +1,11 @@
 require 'pp'
 
-
 require 'eventide/postgres'
+require 'consumer/postgres'
+require 'component_host'
+
 require 'sequel'
+
 require 'file_transfer/client'
 
 require 'file_transfer_data_export/file'
@@ -11,5 +14,8 @@ require 'file_transfer_data_export/store'
 
 require 'file_transfer_data_export/read_model/postgres'
 require 'file_transfer_data_export/handlers/events'
+
+require 'file_transfer_data_export/consumers/event'
+require 'file_transfer_data_export/start'
 
 
