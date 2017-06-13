12be9a95d9ec0bf745e76d01271d79782925a599
ntl <nathanladd+github@gmail.com>
2017-06-13 14:42:52 -0500

    Event consumer is corrected

--- lib/file_transfer_data_export/consumers/event.rb
+++ lib/file_transfer_data_export/consumers/event.rb
@@ -3,15 +3,7 @@ module FileTransferDataExport
     class Event
       include Consumer::Postgres
 
-      # Specifies handler implementations for handling messages
       handler Handlers::Events
-
-      # Errors are handled by this method. If omitted, the default action when an
-      # error is raised during the dispatching of a message is to re-raise the error
-      def error_raised(error, event_data)
-        #SomeErrorNotificationService.(error)
-        #WriteNotProcessable.(event_data)
-      end
     end
   end
 end
